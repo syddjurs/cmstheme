e4421c7c845a2ff644d64ae995973edd45cde75b
Thomas Thune Hansen <tth@bellcom.dk>
2012-12-14 15:54:00 +0100

    meetings fold in/out update

--- js/jquery.dagsorden.js
+++ js/jquery.dagsorden.js
@@ -1,12 +1,13 @@
 (function ($) {
 var is_visible = false;
-
-$('.agenda-item').hide(); 
-
+$('.agenda-item').hide();
 $('a.agenda-bullet-list').click(function(e) {
-  e.preventDefault();
-  $(this).next('.agenda-item').toggle();
-  $(this).toggleClass('open')
-  return false;
-  });
-}(jQuery));
+e.preventDefault();
+$(this).next('.agenda-item').toggle();
+$(this).toggleClass('open')
+$('a.agenda-bullet-list').not(this).next('.agenda-item').hide();
+$('a.agenda-bullet-list').not(this).removeClass('open');
+var top = $(this).next('.agenda-item').offset().top;
+window.scrollTo(0, top -50);
+});
+}(jQuery)); 

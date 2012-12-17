4dbb191f0e9440d8d4c6735bf7c47da5a4ae31ec
Thomas Thune Hansen <tth@bellcom.dk>
2012-12-17 11:53:54 +0100

    update for meetings view fold in/out and images for sharing

--- js/jquery.dagsorden.js
+++ js/jquery.dagsorden.js
@@ -3,11 +3,11 @@ var is_visible = false;
 $('.agenda-item').hide();
 $('a.agenda-bullet-list').click(function(e) {
 e.preventDefault();
-$(this).next('.agenda-item').toggle();
+$(this).nextAll('.agenda-item').toggle();
 $(this).toggleClass('open')
-$('a.agenda-bullet-list').not(this).next('.agenda-item').hide();
+$('a.agenda-bullet-list').not(this).nextAll('.agenda-item').hide();
 $('a.agenda-bullet-list').not(this).removeClass('open');
-var top = $(this).next('.agenda-item').offset().top;
+var top = $(this).nextAll('.agenda-item').offset().top;
 window.scrollTo(0, top -50);
 });
 }(jQuery)); 

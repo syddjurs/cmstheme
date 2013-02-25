7cec797dbb4c96dd9f06245c847cea3ff2e70949
Anders Bryrup <andersbryrup@gmail.com>
2013-02-25 14:52:46 +0100

    added full width panel

---
+++ panels/layouts/os2web_fuldbredde/panels-os2web-fuldbredde.tpl.php
@@ -0,0 +1,12 @@
+<div class="panel-display panel-node clearfix" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+  <div class="panel-top-container clearfix">
+    <div class="panel-panel grid-9 panel-region-top-right">
+      <div class="inside"><?php print $content['top_right']; ?></div>
+    </div>
+  </div>
+
+  <div class="panel-panel grid-12 alpha panel-region-bottom">
+    <div class="inside"><?php print $content['bottom']; ?></div>
+  </div>
+</div>
+

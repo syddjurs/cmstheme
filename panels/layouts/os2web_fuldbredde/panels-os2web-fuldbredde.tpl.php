9f8c45088de113813ac4bfc56dd379a7e9576f01
Anders Bryrup <andersbryrup@gmail.com>
2013-02-27 13:42:13 +0100

    missing alpha class on element

--- panels/layouts/os2web_fuldbredde/panels-os2web-fuldbredde.tpl.php
+++ panels/layouts/os2web_fuldbredde/panels-os2web-fuldbredde.tpl.php
@@ -1,11 +1,11 @@
 <div class="panel-display panel-node clearfix" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
   <div class="panel-top-container clearfix">
-    <div class="panel-panel grid-9 panel-region-top-right">
+    <div class="panel-panel grid-9 alpha panel-region-top-right">
       <div class="inside"><?php print $content['top_right']; ?></div>
     </div>
   </div>
 
-  <div class="panel-panel grid-12 alpha panel-region-bottom">
+  <div class="panel-panel grid-12 panel-region-bottom">
     <div class="inside"><?php print $content['bottom']; ?></div>
   </div>
 </div>
